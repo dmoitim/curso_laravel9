c755a4aba6f43170724ec5ea45c87aedff27bde0
Devair Luiz Moitim <dmoitim@gmail.com>
2022-03-22 22:42:06 -0300

    Alteração do usuário

--- app/Http/Requests/StoreUpdateUserFormRequest.php
+++ app/Http/Requests/StoreUpdateUserFormRequest.php
@@ -23,12 +23,15 @@ class StoreUpdateUserFormRequest extends FormRequest
      */
     public function rules()
     {
-        return [
+        // Recupera o id do usuário informado na URL
+        $id = $this->id ?? '';
+
+        $rules = [
             'name' => 'required|string|max:255|min:3',
             'email' => [
                 'required',
                 'email',
-                'unique:users'
+                "unique:users,email,{$id},id"
             ],
             'password' => [
                 'required',
@@ -36,5 +39,15 @@ class StoreUpdateUserFormRequest extends FormRequest
                 'max:15'
             ]
         ];
+
+        if ($this->method('PUT')) {
+            $rules['password'] = [
+                'nullable',
+                'min:6',
+                'max:15'
+            ];
+        }
+
+        return $rules;
     }
 }
