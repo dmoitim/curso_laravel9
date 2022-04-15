0825d64ea3a9ada1767a290a47ccc7f3c35d0a6a
Devair Luiz Moitim <dmoitim@gmail.com>
2022-04-15 12:00:23 -0300

    Implementação da autenticação

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-
-Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
-Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
-Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::get('/users/{user}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
-Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
+Route::middleware(['auth'])->group(function() {
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    
+    Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
+    Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+    Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::get('/users/{user}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
+    Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__ . '/auth.php';
